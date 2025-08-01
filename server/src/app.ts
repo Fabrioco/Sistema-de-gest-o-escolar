1651c44e20a0c963fb6ed66b4bdce459d57e223d
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-08-01 13:03:46 -0300

    feat(app): adicionei o errorZod middleware

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,10 @@
 import express from "express";
 import indexRoutes from "./modules/index.routes";
+import { Middleware } from "./commons/middlewares";
 
 export const app = express();
 
 app.use(express.json());
 app.use(indexRoutes);
+
+app.use(Middleware.errorZod)
