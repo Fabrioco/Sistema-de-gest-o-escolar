73a0140e417203409c1e4ed13ff5d526696ec1b9
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-08-01 02:34:58 -0300

    cleanup(server): separei para deixar apenas ligações com banco de ligar o servidor

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,8 @@
-import express from "express";
 import dotenv from "dotenv";
+import { app } from "./app";
 
 dotenv.config();
 
-const app = express();
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
