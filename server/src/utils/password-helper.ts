dd37b144bc4d229b9ad13c172aac5bc917719183
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-08-01 02:34:02 -0300

    feat(passwordHelper): criei o function de comparePassword

--- server/src/utils/password-helper.ts
+++ server/src/utils/password-helper.ts
@@ -1,8 +1,16 @@
-import bcrypt from "bcrypt"
+import bcrypt from "bcrypt";
 
 export class PasswordHelper {
-    static async hashPassword(password:string) {
-        const salt = bcrypt.genSaltSync(10);
-        return await bcrypt.hash(password, salt);
+  static async hashPassword(password: string) {
+    const salt = bcrypt.genSaltSync(10);
+    return await bcrypt.hash(password, salt);
+  }
+
+  static async comparePassword(password: string, hash: string) {
+    try {
+      return await bcrypt.compare(password, hash);
+    } catch (error) {
+      return
     }
+  }
 }
