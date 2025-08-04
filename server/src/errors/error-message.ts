2a99434814b735363437f24f907f6c9da4dbdfdf
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-08-03 22:00:01 -0300

    feat(errors): adicionei a mensagem de token

--- server/src/errors/error-message.ts
+++ server/src/errors/error-message.ts
@@ -1,4 +1,6 @@
 export default {
     CREDENTIAL_INVALID: "Credenciais inválidas",
     USER_NOT_FOUND: "Usuário não encontrado",
+    TOKEN_EXPIRED: "Token expirado",
+    TOKEN_INVALID: "Token inválido"
 }
